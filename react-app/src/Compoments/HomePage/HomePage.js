307afdbaad814417f94925ca220b0d7a213548a0
skymrsn <46433941+skymrsn@users.noreply.github.com>
2021-02-07 21:04:58 +0800

    Update HomePage.js

--- react-app/src/Compoments/HomePage/HomePage.js
+++ react-app/src/Compoments/HomePage/HomePage.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 const HomePage = (props)=>{
     return (
-        <div className="App">
+        <div className="page">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>
-                    Hello from BetaGo          
-                </h1>
+         
             </header>
         </div>
     );
