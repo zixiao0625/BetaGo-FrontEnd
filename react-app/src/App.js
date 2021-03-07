3c65d2c15cb83482ee31ddf03a78583cc2b48fac
Nicholas Xu <tichx@uw.edu>
2021-03-08 05:06:20 +0800

    added a raw version

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,7 @@
 import HomePage from './Compoments/HomePage/HomePage.js'
 import {RoomPage,Room} from './Compoments/RoomPage/RoomPage.js'
 import Invitation from './Compoments/Invitation/Invitation.js'
+import RoomListPage from './Compoments/RoomListPage/index.jsx'
 import Login from './Compoments/Login/Login.js'
 import React, { useState, useEffect } from "react";
 import {HashRouter, Redirect, Route, Switch} from 'react-router-dom';
@@ -16,7 +17,7 @@ function App() {
   return (
     <HashRouter>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={RoomListPage} />
         <Route exact path="/room" component={HomePage} />
         <Route exact path="/room/:roomid" component={Room} />
         <Route exact path="/invitation" component={Invitation} />
