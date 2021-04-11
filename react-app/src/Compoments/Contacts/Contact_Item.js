40134489e5ea6b12e4ef71b4869814d784ce3e2d
skymrsn <46433941+skymrsn@users.noreply.github.com>
2021-04-11 16:43:15 +0800

    profile card embeded

--- react-app/src/Compoments/Contacts/Contact_Item.js
+++ react-app/src/Compoments/Contacts/Contact_Item.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import "./Contacts.css";
 import demoPic from '../../Icons/avatar.jpg'
+import Profile from './Profile.js'
+import { Popover } from '@material-ui/core';
 
 const ContactItem = (client_ID, ...props) => {
 
@@ -8,6 +10,7 @@ const ContactItem = (client_ID, ...props) => {
   const [avatar, setAvatar] = useState('')
   const [status, setStatus] = useState('')
   const [userBio, setUserBio] = useState('')
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const getUserInfo = async () => {
       // get user name
@@ -26,18 +29,60 @@ const ContactItem = (client_ID, ...props) => {
 
   }
 
-  // const userName = getUserInfo()['userName']['S']
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
   useEffect(() => {
     getUserInfo()
   })
 
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+
   return (
       <div className="" key={userName}>
-        <div className="each-person">
+        <div className="each-person" onClick={handleClick}>
           <img className="picture" src = {demoPic} alt="contact_list" style={{position: 'inline'}}/> {userName}
           <span style={{position: 'relative', top: '15px'}}>{status}</span>
           <button className="button">Join</button>
         </div>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          style={{height: '600px'}}
+        >
+          <Profile
+            profilePic="https://anima-uploads.s3.amazonaws.com/projects/603f96b6a6fa0860cddc1b17/releases/607298fb5ec03fa5072c8f31/img/profile-pic@2x.png"
+            name="Nicholas Xu"
+            bio_text={
+              <>
+                Who said I don’t party
+                <br />
+                UW 21 | Seattle | Shenzhen
+                <br />A product guy en route
+              </>
+            }
+            addFriend="+ Add Friend"
+            style={{height: 'auto'}}
+          />
+        </Popover>
       </div>
     )
 }
