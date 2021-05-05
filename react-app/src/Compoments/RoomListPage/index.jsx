9e6cf995a4afbe11b3aa78db5c134171435cb902
skymrsn <46433941+skymrsn@users.noreply.github.com>
2021-05-06 00:01:25 +0800

    UI fix bug

--- react-app/src/Compoments/RoomListPage/index.jsx
+++ react-app/src/Compoments/RoomListPage/index.jsx
@@ -145,14 +145,15 @@ const RoomListPage = () => {
     <div>
       <header className="p-3 mb-3 border-bottom">
         <div className="container">
-          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          {/* <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start"> */}
+            <Box display='flex' flexDirection='row' justifyContent='space-between' style={{ width: '100%'}} >
             <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
               <svg className="bi me-2" width={40} height={32}><use xlinkHref="#bootstrap" /></svg>
             </a>
             <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
               <li><a href="#" className="nav-link px-2 link-secondary">Home</a></li>
             </ul>     
-            <div className="dropdown text-end">
+            <div className="dropdown text-end" style={{ marginTop: '7px' }}>
               <a 
                 href="#" 
                 className="d-block link-dark text-decoration-none dropdown-toggle" 
@@ -191,7 +192,8 @@ const RoomListPage = () => {
                 </div>
               </Popover>
             </div>
-          </div>
+            </Box>
+          {/* </div> */}
         </div>
       </header>
 
@@ -244,7 +246,7 @@ const RoomListPage = () => {
           <UploadCard setAvatar={setAvatar} setUserName={setUserName} setUserBio={setUserBio} />
         </Popover>
       <div className="middlePage">
-        <div className="RoomList">
+        <div className="RoomList" style={{marginLeft: '10%'}}>
           <RoomCardPage/>
           <img className="plusButton" src={plusButton} alt="plus" onClick={handleCreateRoom} />
         </div>
